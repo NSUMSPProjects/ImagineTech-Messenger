51d8478953f2b473cb46e1996bba58c40732b153
Abu Mohammad Shabbir Khan <s.h.a.b.b.i.r@live.com>
2016-10-10 03:18:11 +0600

    Implemented Project Skeletons

---
+++ ImagineTechMessenger/MessengerServer/MessengerService.cs
@@ -0,0 +1,21 @@
+﻿using MessengerInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MessengerServer
+{
+    // InstanceContextMode is single so that all clients will have access to one instance and everyone can know who
+    // is connected to the service
+    // ConcurrencyMode is multiple because we want a multithreaded server
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.Single)]
+    public class MessengerService : IMessengerService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
